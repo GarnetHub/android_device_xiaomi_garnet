0413fe7cfee3035371ad6c85c15b534da72295b0
SenseiDEVLinux <klimashevskiymaxim2003@gmail.com>
2025-09-28 22:18:30 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -72,7 +72,7 @@ DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(DEVICE_PATH)/configs/hidl/vendor.dolby.media.c2.xml \
     $(DEVICE_PATH)/configs/hidl/vendor.dolby.hardware.dms@2.0-service.xml \
-	$(DEVICE_PATH)/configs/hidl/dolby_framework_matrix.xml \
+	$(DEVICE_PATH)/configs/hidl/device_framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
