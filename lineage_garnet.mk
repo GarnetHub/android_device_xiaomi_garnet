2152a5fd0b4c017771928a6db02a7c3399f7e811
SenseiDEVLinux <klimashevskiymaxim2003@gmail.com>
2025-10-13 19:48:18 +0300

    Update lineage_garnet.mk

--- lineage_garnet.mk
+++ lineage_garnet.mk
@@ -15,6 +15,8 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # Inherit from garnet device
 $(call inherit-product, device/xiaomi/garnet/device.mk)
 
+WITH_GMS := true
+
 PRODUCT_NAME := lineage_garnet
 PRODUCT_DEVICE := garnet
 PRODUCT_MANUFACTURER := Xiaomi
